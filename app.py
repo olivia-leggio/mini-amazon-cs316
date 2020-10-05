cb72f8ad9b4f3a70d411c658a7cc6698c8a4ffbd
paulsab17 <paulsab17@gmail.com>
2020-10-05 09:53:41 -0400

    add form to add an item, and use base.html format

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect, url_for, render_template
 from models import Item
 from database import db_session
 
@@ -16,6 +16,20 @@ def database():
     'database.html',
     items = Item.query.all()
   )
+
+@app.route('/add_item')
+def add_item():
+  name = request.args.get("name")
+  brand = request.args.get("brand")
+  color = request.args.get("color")
+  size = request.args.get("size")
+
+  item = Item(name, brand, color, size)
+  db_session.add(item)
+  db_session.commit()
+
+  return redirect(url_for('database'))
+
 
 @app.route('/account')
 def account():
