6031d302b6cd4e460d0c98f83b2ea8f3942d4c37
student <student@macbookair2-m.local>
2020-10-19 01:47:40 -0400

    added login page

--- app.py
+++ app.py
@@ -11,6 +11,17 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 def index():
     return render_template('index.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('browse'))
+    return render_template('login.html', error=error)
+
+
 @app.route('/browse')
 def browse():
   cat = request.args.get('cat','ALL')
